9cbe3a497100beb60f41e1026cb8c0b07442d4ec
Bessa <bessa.vitor06@gmail.com>
2024-08-15 17:35:22 -0300

    Finalização

---
+++ lib/hangman.rb
@@ -0,0 +1,80 @@
+require "csv"
+
+module Hangman
+  class Game
+    def pick_words
+      words = File.read("words.txt")
+      words = words.split
+      array_word = []
+      words.each do |word|
+        array_word.push(word) if word.size.between?(5, 12)
+      end
+
+      array_word.sample(1).join.upcase
+    end
+
+    attr_accessor :lifes, :player_word, :word
+
+    def initialize
+      @word = pick_words
+      @player_word = "_" * @word.size
+      @lifes = 6
+    end
+
+    def check_player_word(character)
+      count = 0
+      indexs = []
+      @word.each_char.with_index do |char, index|
+        if character == char
+          count += 1
+          indexs.push(index)
+        end
+      end
+      @lifes -= 1 if count.zero?
+      puts
+      indexs.each do |idx|
+        @player_word[idx] = character
+      end
+      print_word
+    end
+
+    def print_word
+      puts "#{@player_word.colorize(:green)},  #{@lifes} lifes Reamining"
+    end
+
+    def check_winner
+      if @lifes == 0
+        puts "#{'You loose'.colorize(:red)}. press Enter to back to menu"
+        gets
+      end
+      unless @player_word.include?("_")
+        puts "Congratulations, you win the game\n\n".colorize(color: :yellow, mode: :bold)
+        puts "Press enter to continue".colorize(color: :red)
+        gets
+        return false
+      end
+      true
+    end
+
+    def save_game
+      data = "word,#{@word}\nplayer_word,#{player_word}\nlifes,#{@lifes}"
+      path = "data.csv"
+      File.write(path, data)
+      exit
+    end
+  end
+
+  class Player
+    def choose_letter(game)
+      puts "Enter a character or digit 'SAVE' to save the game and exit:"
+      chara = gets.chomp.upcase
+      game.save_game if chara == "SAVE"
+      while chara.size != 1
+        puts "Enter with #{'ONE'.colorize(:red)} character or digit 'SAVE' to save the game and exit:"
+        chara = gets.chomp.upcase
+        game.save_game if chara == "SAVE"
+      end
+      chara
+    end
+  end
+end
---
+++ main.rb
@@ -0,0 +1,71 @@
+require_relative "lib/hangman"
+require "colorize"
+
+def intro
+  puts "Hangman".center(62, "*").colorize(color: :blue, mode: :bold)
+end
+
+def menu
+  puts "1 - Play game\n2 - How to play\n3 - Load Game\n4 - Exit"
+  opcao = gets.to_i
+  unless [1, 2, 3, 4].include?(opcao)
+    puts "Wrong Option. Try again".colorize(color: :red, mode: :bold)
+    puts "1 - Play the game\n2 - How to play\n 3 - Load Game\n4 - Exit"
+    opcao = gets.to_i
+  end
+  opcao
+end
+
+def how_to_play
+  puts "You must guess the word before you lives #{'run out'.colorize(:red)}\n\n"
+  puts "Press enter to continue".colorize(color: :red)
+  gets
+end
+
+def start_game(game, player)
+  while game.check_winner && game.lifes.positive?
+    system "clear"
+    intro
+    game.print_word
+    game.check_player_word(player.choose_letter(game))
+  end
+end
+
+def load_game(game)
+  file = CSV.open("data.csv")
+  file.each do |row|
+    if row[0] == "word"
+      game.word = row[1].to_s
+    elsif row[0] == "player_word"
+      game.player_word = row[1].to_s
+    else
+      game.lifes = row[1].to_i
+    end
+  end
+end
+
+include Hangman
+
+in_game = true
+
+while in_game == true
+  system "clear"
+  intro
+  opcao = menu
+  case opcao
+  when 1
+    game = Game.new
+    player = Player.new
+    start_game(game, player)
+  when 2
+    how_to_play
+  when 3
+    game = Game.new
+    player = Player.new
+    load_game(game)
+    start_game(game, player)
+    gets
+  else
+    exit
+  end
+end
